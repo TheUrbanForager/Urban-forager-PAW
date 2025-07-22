6c3e25884481c72f209de0697aeff58020d5a6dd
TheUrbanForager <christomp81@gmail.com>
2025-07-22 15:43:33 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('urban-forager-v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/manifest.json',
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
